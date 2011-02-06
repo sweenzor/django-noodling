4a6fb2d4c803803c7ef1c1db95427bab7e9f2a7e
Matt Sweeney <mattswe@gmail.com>
2011-02-06 11:58:15 -0800

    added detail view

--- noodles/polls/views.py
+++ noodles/polls/views.py
@@ -1,18 +1,18 @@
 # Create your views here.
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from polls.models import Poll
-from django.http import HttpResponse 
+from django.http import Http404
 
 def index(request):
     latest_poll_list1 = Poll.objects.all().order_by('-pub_data')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list1,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list1})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return renter_to_response('polls/detaul.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
