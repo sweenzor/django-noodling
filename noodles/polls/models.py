9a057a2b538d42021b3b15eba7efae9f5a160837
Matt Sweeney <mattswe@gmail.com>
2011-02-05 17:20:57 -0800

    extending model classes

--- noodles/polls/models.py
+++ noodles/polls/models.py
@@ -6,8 +6,12 @@ from django.db import models
 class Poll(models.Model):
 	question = models.CharField(max_length=200)
 	pub_data = models.DateTimeField('date published')
+	def __unicode__(self):
+		return self.question
 
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice = models.CharField(max_length=200)
 	votes = models.IntegerField()
+	def __unicode__(self):
+		return self.choice
