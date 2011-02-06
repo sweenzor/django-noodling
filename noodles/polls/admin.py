82e4be1929d445b5cb493b7a0384f8949b4e4a42
Matt Sweeney <mattswe@gmail.com>
2011-02-05 19:47:24 -0800

    views

--- noodles/polls/admin.py
+++ noodles/polls/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 
 class PollAdmin(admin.ModelAdmin):
-	fields = ['pub_date','question']
+	fields = ['pub_data','question']
 
 admin.site.register(Poll, PollAdmin)
--- noodles/urls.py
+++ noodles/urls.py
@@ -5,6 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^polls/$', 'polls.views.index'),
+    (r'^polls/(?P<poll_id>\d+)/$', 'polls.view.detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
     # Example:
     # (r'^noodles/', include('noodles.foo.urls')),
 
