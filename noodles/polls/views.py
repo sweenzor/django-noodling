9ab9a083be6c185ed536aa0b8ea400b8f9bbb92d
Matt Sweeney <mattswe@gmail.com>
2011-02-06 11:19:59 -0800

    working template

--- noodles/polls/views.py
+++ noodles/polls/views.py
@@ -4,10 +4,10 @@ from polls.models import Poll
 from django.http import HttpResponse 
 
 def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    latest_poll_list1 = Poll.objects.all().order_by('-pub_data')[:5]
     t = loader.get_template('polls/index.html')
     c = Context({
-        'latest_poll_list': latest_poll_list,
+        'latest_poll_list': latest_poll_list1,
     })
     return HttpResponse(t.render(c))
 
